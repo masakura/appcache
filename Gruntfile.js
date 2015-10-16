4b35f4eff6868eff2c3825f6d4d8f3a520c58f6a
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2015-10-16 10:09:23 +0900

    add copy appcache manifest setting

--- Gruntfile.js
+++ Gruntfile.js
@@ -366,7 +366,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'sample.appcache'
           ]
         }, {
           expand: true,
